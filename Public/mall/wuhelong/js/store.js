bbc3ac30615e25271fe96d7f010fff2c71ff7802
gaoyuqi1991 <583110724@qq.com>
2016-05-04 15:39:48 +0800

    gyq商铺
    02020202商铺
    
    gyq会员设置里面
    
    会员设置里面

--- Public/mall/wuhelong/js/store.js
+++ Public/mall/wuhelong/js/store.js
@@ -22,4 +22,28 @@ $(function(){
 		intro.stop().slideUp("fast"); 
 	});
 
+
+    $(function(){   
+        //缩略图对应大图
+        $('.smallImg').hover(function(){
+            $(this).parent().parent().parent().parent().parent('.goodsList-wrap').find('.jqzoom img').attr("src",$(this).attr("src"));
+            $(this).parent().parent().parent().parent().parent('.goodsList-wrap').find('.jqzoom img').attr("jqimg",$(this).attr("bimg"));
+        });
+        
+        //图片下方公司出现
+        $('.spec-preview').hover(function(){
+            $(this).find('.spec-bottom').toggle();
+        })
+
+        //缩略图的边框变化
+        $('.spec-scroll .items li').hover(function(){
+            $(this).addClass('active').siblings().removeClass('active');
+        })
+    });
+    /*收藏商品*/
+    function addCollect(product_id){
+        alert(product_id)
+    }
+
+
 });
